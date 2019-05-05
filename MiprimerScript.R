8b369314f487a182fd6a3678ae83ebd124a5516b
DianaLOA <dlopezav@gmail.com>
2019-05-05 18:16:59 -0400

    Ejercicios de loop e if

--- MiprimerScript.R
+++ MiprimerScript.R
@@ -49,3 +49,77 @@ if(r1>=r2){
  abs( areaAnillo(r1,r2))
 }
 
+hola <- "h"
+grepl('[A-Za-z]',hola)
+
+#Listas
+lista1<-list(1,2,3)
+lista2<-c(lista1,7,8)
+
+lista3<-lista2[c(-2,-3)]
+
+#Agrego columnas
+Lst <- list(hombre = "Pedro", mujer = "María",
+            casados = TRUE,
+            número.hijos = 3, edad.hijos = c(4, 7, 9))
+
+#Obtengo el valor que acompaña a hombre
+Lst$hombre
+
+
+##For/Loop
+
+for(i in 1:50){
+  print(i)
+}
+
+seq(1,50,by=5)
+
+
+
+#Clase sabado 04-05-2019
+elValor<-"hola"
+
+check2valor<-function(elValor){
+  if(grepl("[A-Za-z]", elValor)){
+   print("Esto es texto")
+  
+}else if(elValor>0){
+    
+      print("Es positivo")
+}else if(elValor<0){
+
+   print("Es negativo")
+    
+}else{
+  print("Es cero")  
+  
+}
+}
+
+
+
+for(w in 1:length(lista2)){
+  
+  check2valor(lista[w])
+}
+
+
+##Mientras hago esto imprime la condicion
+
+variable<-0
+while(TRUE){
+  print(paste("hola mundo",variable))
+  variable<-variable+1
+  
+}
+
+variable<-0
+while(variable<1000){
+  print(paste("hola mundo",variable))
+  variable<-variable+1
+  
+}
+
+
+
